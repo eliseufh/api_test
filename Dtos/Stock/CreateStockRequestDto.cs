916a11a0e502ddbb11b121a47dee735b9c5559c7
Eliseu <eliseusilva.info@gmail.com>
2024-08-13 14:16:40 +0100

    Added CreateStockRequestDto for creating new stocks

---
+++ Dtos/Stock/CreateStockRequestDto.cs
@@ -0,0 +1,11 @@
+namespace api.Dtos.Stock;
+
+public class CreateStockRequestDto
+{
+    public string Symbol { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public decimal Purchase { get; set; }
+    public decimal LasDiv { get; set; }
+    public string Industry { get; set; } = string.Empty;
+    public long MarketCap { get; set; }
+}
