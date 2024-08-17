b67d1d7751415a901c8505b097cc2942bbe7ce11
Eliseu <eliseusilva.info@gmail.com>
2024-08-17 18:37:51 +0100

    Refactor to Repository

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.Dtos.Stock;
 using api.Interfaces;
 using DefaultNamespace;
 using Microsoft.EntityFrameworkCore;
@@ -13,8 +14,51 @@ public class StockRepository : IStockRepository
         _context = context;
     }
     
-    public Task<List<Stock>> GetAllAsync()
+    public async Task<List<Stock>> GetAllAsync()
     {
-        return _context.Stocks.ToListAsync();
+        return await _context.Stocks.ToListAsync();
+    }
+
+    public async Task<Stock?> GetByIdAsync(int id)
+    {
+        return await _context.Stocks.FindAsync(id);
+    }
+
+    public async Task<Stock> CreateAsync(Stock stockModel)
+    {
+        await _context.Stocks.AddAsync(stockModel);
+        await _context.SaveChangesAsync();
+        return stockModel;
+    }
+
+    public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+    {
+        var existingStock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (existingStock == null)
+            return null;
+        
+        existingStock.Symbol = stockDto.Symbol;
+        existingStock.CompanyName = stockDto.CompanyName;
+        existingStock.Purchase = stockDto.Purchase;
+        existingStock.LasDiv = stockDto.LasDiv;
+        existingStock.Industry = stockDto.Industry;
+        existingStock.MarketCap = stockDto.MarketCap;
+        
+        await _context.SaveChangesAsync();
+
+        return existingStock;
+    }
+
+    public async Task<Stock?> DeleteAsync(int id)
+    {
+        var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (stockModel == null)
+            return null;
+        
+        _context.Stocks.Remove(stockModel);
+        await _context.SaveChangesAsync();
+        return stockModel;
     }
 }
